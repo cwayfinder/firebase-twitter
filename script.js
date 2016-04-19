2dd8de957702c87bcb53ee8633fd841a6be8acf3
Taras Hupalo <taras.hupalo@gmail.com>
2016-04-19 17:44:06 +0300

    Sync with original

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-(function () {
+(function() {
   // Config
   var timelinePageCount = 3;
 
   // Templating functions
-  var setUsers = function (users) {
+  var setUsers = function(users) {
       console.info('called setUsers with these users:', users);
       $('#user-select').html(_.template($('#user-select-template').html())({
         users: users
       })).find('select').on('change', handleUserChange).val(Object.keys(users)[0]).trigger('change');
 
     },
-    setTimeline = function (timeline, userKey, buttons, callback) {
+    setTimeline = function(timeline, userKey, buttons, callback) {
       console.info('called setTimeline with this timeline:', timeline);
       $('#user-timeline').html(_.template($('#user-timeline-template').html())({
         timeline: timeline,
@@ -24,18 +24,18 @@
         callback();
       }
     },
-    setFollowing = function (following) {
+    setFollowing = function(following) {
       console.info('called setFollowing with this following:', following);
 
       $('#user-following').html(_.template($('#user-following-template').html())({
         following: following
-      }))
+      }));
     },
-    setTweetBox = function (user) {
+    setTweetBox = function(user) {
       console.info('called setTweetBox with this user:', user);
       $('#user-tweet-box').html(_.template($('#user-tweet-box-template').html())({
         user: user
-      })).find('textarea').on('keyup', function (e) {
+      })).find('textarea').on('keyup', function(e) {
         var characterCount = $(e.target).val().length,
           tweetLength = $('#tweet-length'),
           tweetButton = $('#tweet-button');
@@ -55,67 +55,76 @@
       });
     };
 
-  var firebaseRoot = 'https://flickering-fire-5027.firebaseio.com/twitterClone/';
-  var usersRef = new Firebase(firebaseRoot + 'users');
-  var userObjectsRef = new Firebase(firebaseRoot + 'userObjects');
+  var firebaseRoot = "https://flickering-fire-5027.firebaseio.com/twitterClone/",
+    usersRef = new Firebase(firebaseRoot + 'users'),
+    userObjectsRef = new Firebase(firebaseRoot + 'userObjects');
 
-  usersRef.once('value', function (snap) {
-    setUsers(snap.val);
-  });
+  usersRef.once('value', function(snap) {
+    setUsers(snap.val());
+  })
 
-  var flatten = function(tweets) {
-    var keys = Object.keys(tweets),
-      i = keys.length,
-      result = [],
-      tweet;
-    while(i--) {
-      tweet = tweets[key[i]];
-      tweet.key = keys[i];
-      result.unshift(tweet);
-    }
+  var timelineRef,
+    timelineHandler,
+    tweetBoxClickHandler,
+    userRef,
+    userHandler,
+    tweetsRef,
+    tweetAddedHandler,
+    stopListening = function() {
+      if(typeof timelineRef === 'object' && typeof timelineHandler) {
+        timelineRef.off('value', timelineHandler);
+      }
+      if(typeof userRef === 'object' && typeof userHandler) {
+        userRef.off('value', userHandler);
+      }
+      if(typeof tweetsRef === 'object' && typeof tweetAddedHandler) {
+        tweetsRef.off('child_added', tweetAddedHandler);
+      }
 
-    return result;
-  };
+    },
+    flatten = function(tweets) {
+      var keys = Object.keys(tweets),
+        i = keys.length,
+        result = [],
+        tweet;
+      while (i--) {
+        tweet = tweets[keys[i]];
+        tweet.key = keys[i];
+        result.unshift(tweet);
+      }
+      return result;
+    };
 
-  var timelineRef, userRef;
-  var timelineHandler, userHandler;
-
-  var stopListening = function() {
-    if (typeof timelineRef === 'object' && typeof timelineHandler) {
-      timelineRef.off('value', timelineHandler);
-    }
-    if (typeof userRef === 'object' && typeof userHandler) {
-      userRef.off('value', userHandler);
-    }
-  };
-
-  var tweetBoxClickHandler;
-
-  var handleUserChange = function (e) {
+  var handleUserChange = function(e) {
     var userKey = $(e.target).val();
 
     stopListening();
 
     if (userKey) {
+
       timelineRef = userObjectsRef.child('timeline').child(userKey);
       timelineHandler = timelineRef.on('value', function(snap) {
-        setTimeline(flatten(snap.val()).reverse(), userKey);
-      });
+        setTimeline(flatten(snap.val()).reverse(), userKey)
+      })
 
       userRef = usersRef.child(userKey);
+
       userHandler = userRef.on('value', function(snap) {
         setTweetBox(snap.val());
       });
+
       userObjectsRef.child('following').child(userKey).once('value', function(snap) {
         setFollowing(snap.val());
-      });
+      })
+
 
       var userTweetBox = $('#user-tweet-box');
 
-      // Prevent duplicate registration
-      if(typeof tweetBoxClickHandler === 'function') {
+      // Prevent duplicate registration of the tweetBoxClickHandler listener
+      if (typeof tweetBoxClickHandler === 'function') {
         userTweetBox.off('click', 'button', tweetBoxClickHandler);
       }
+
       tweetBoxClickHandler = function(e) {
         e.preventDefault();
 
@@ -125,32 +134,78 @@
         };
 
         userObjectsRef.child('tweets').child(userKey).push(tweet, function(err) {
-          if (err) {
-            console.warn('Error!', err);
-          } else {
-            userRef.once('value', function(snap) {
-              var user = snap.val();
-              userObjectsRef.child('timeline').child(userKey).push({
-                created: tweet.created,
-                text: tweet.text,
-                userKey: userKey,
-                user: {
-                  email: user.email,
-                  key: userKey,
-                  name: user.name,
-                  username: user.username
-                }
-              });
-            });
+          if(err) {
+            console.warn('error!', err);
           }
-        });
-      };
+        })
+      }
+
       userTweetBox.on('click', 'button', tweetBoxClickHandler);
+
+      tweetsRef = userObjectsRef.child('tweets').child(userKey);
+
+      tweetAddedHandler = tweetsRef.on('child_added', function(snap) {
+        var tweet = snap.val(),
+          tweetRef = snap.ref();
+
+        if(!tweet.fannedOut) {
+          usersRef.child(userKey).once('value', function(snap) {
+            var user = snap.val();
+            var tweetUser = {
+              email: user.email,
+              key: snap.key(),
+              name: user.name,
+              username: user.username
+            };
+
+            userObjectsRef.child('followers').child(userKey).child('list')
+              .once('value', function(snap) {
+                var i = snap.numChildren();
+                snap.forEach(function(childSnap) {
+                  var follower = childSnap.val();
+                  tweet.tweetKey = tweetRef.key();
+                  tweet.user = tweetUser;
+                  tweet.userKey = tweetUser.key;
+
+                  userObjectsRef.child('timeline').child(follower.key)
+                    .push(tweet, function(err) {
+                      i -= 1;
+                      if(i <= 0) {
+                        tweetRef.child('fannedOut').set(true);
+                      }
+                    })
+                })
+              })
+          })
+        }
+      })
+
     } else {
       setTweetBox({});
       setTimeline({});
       setFollowing({});
     }
+
+    $('#user-timeline').on('click', 'button.remove-tweet', function(e) {
+      var target = $(e.target),
+        userKey = target.attr('user-key'),
+        tweetKey = target.attr('tweet-key');
+
+      userObjectsRef.child('tweets').child(userKey).child(tweetKey)
+        .remove(function(err) {
+          if(err) {
+            console.warn('Tweet deletion error', err);
+          } else {
+            usersRef.child(userKey).once('value', function(snap) {
+              var user = snap.val(),
+                userRef = snap.ref();
+
+              userRef.update({tweetCount: Math.max(0, (user.tweetCount || 0) - 1)});
+            })
+          }
+        })
+
+    })
 
   };
 
