450c5e3e89b0e5ceacb0c9be2b151f405a22c385
Taras Hupalo <taras.hupalo@gmail.com>
2016-04-19 14:31:11 +0300

    Update user list only once (optimization)

--- script.js
+++ script.js
@@ -55,7 +55,7 @@
   var firebaseRoot = 'https://flickering-fire-5027.firebaseio.com/twitterClone/';
   var usersRef = new Firebase(firebaseRoot + 'users');
 
-  usersRef.on('value', function (snap) {
+  usersRef.once('value', function (snap) {
     setUsers(snap.val);
   });
 
